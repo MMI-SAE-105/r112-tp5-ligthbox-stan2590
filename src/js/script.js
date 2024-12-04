06bbbdb7e635b656dd9031ad92f7062a0adda813
stan2590 <evan.gay@edu.univ-fcomte.fr>
2024-12-04 16:46:18 +0100

    ferme et ouvir image

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,21 @@
 // *** LIGHTBOX ***
 
+document.addEventListener("DOMContentLoaded", () => {
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImage = lightbox.querySelector("img");
+
+    document.querySelectorAll("ul li img").forEach((img) => {
+        img.addEventListener("click", () => {
+            const fullImgSrc = img.getAttribute("data-full-img");
+            lightboxImage.src = fullImgSrc;
+            lightbox.classList.add("open");
+        });
+    });
+
+    lightbox.addEventListener("click", (e) => {
+        if (e.target === lightbox || e.target.tagName === "DIALOG") {
+            lightbox.classList.remove("open");
+            lightboxImage.src = ""; 
+        }
+    });
+});
